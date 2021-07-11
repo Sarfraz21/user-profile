da781c07fbcc81bb0ea7fb7ccbe716b2d530b47d
Sarfraz Hussain <sarfaraz.hussain856@gmail.com>
2021-07-11 22:25:59 +0530

    "first angular project"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+  import { FormsModule } from '@angular/forms';
+  import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './users/employee/employee.component';
+import { EmployeeService } from './users/employee.service';
+import { HomeComponent } from './home/home.component';
+import { SortNameComponent } from './sort-name/sort-name.component';
+import { EmployeelistComponent } from './users/employeelist/employeelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    HomeComponent,
+    SortNameComponent,
+    EmployeelistComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sort-name/sort-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-name',
+  templateUrl: './sort-name.component.html',
+  styleUrls: ['./sort-name.component.css']
+})
+export class SortNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
